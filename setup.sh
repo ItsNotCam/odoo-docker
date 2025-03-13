5e4ed6ff3bf6f2e839e4858125dc31a6e4ab8328
ItsNotCam <camallanyoung@gmail.com>
2025-03-12 23:08:44 -0400

    some changes i forgot sorry

--- setup.sh
+++ setup.sh
@@ -1,5 +1,32 @@
-mkdir -p docker-volumes/nginx_logs docker-volumes/postgres_data
+mkdir -p \
+  docker-volumes/nginx_logs    \
+	docker-volumes/postgres_data \
+	docker-volumes/odoo_addons
+
+read -sp "Enter root password for odoo: " ROOT_PWD
+echo $ROOT_PWD > pwd.tmp
+echo
+
+read -p "Do you want to encrypt this password file? (y/n): " DO_ENCRYPT
+if [[ $DO_ENCRYPT == "y" ]]; then
+	read -sp "Enter the password to your user account: " USER_PWD
+	echo
+	openssl enc -aes-256 -iter 100000 -salt -in pwd.tmp -out odoo/odoo_root_pwd.txt -pass pass:$USER_PWD
+	rm pwd.tmp
+else
+	mv pwd.tmp odoo/odoo_root_pwd.txt
+fi
+echo
 
 cd odoo
-docker build --no-cache -t odoo:latest -f odoo.Dockerfile.dev .
+docker build \
+	--build-arg ROOT_PASSWORD=$ROOT_PWD \
+	--no-cache \
+	-t odoo:latest \
+	-f odoo.Dockerfile.dev .
 cd ..
+
+# Cleanup
+unset ROOT_PWD
+unset USER_PWD
+unset DO_ENCRYPT
